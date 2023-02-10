175178cb49e6d3d4370de2be5d20dbca7cf937b5
Demond Harris <Dae@demonds-mbp.myfiosgateway.com>
2023-02-10 12:44:52 -0500

    stringUpdates

---
+++ stringsAssignment.js
@@ -0,0 +1,19 @@
+// Problem One
+//function checking(str){
+//if (str === "") {
+//    console.log("Empty String")
+//}
+//else {
+//    console.log("Not Empty String")
+//}
+//}
+
+//Problem Two
+
+let str = "Hello. My name is April."
+let chars = str.split('');
+let words = str.split(' ');
+let noLetterE = str.split('e');
+    console.log(chars)
+    console.log(words)
+    console.log(noLetterE)
